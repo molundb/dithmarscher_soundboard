6dafba63b58fcb9b350a805d117a880a5f484647
Martin Lundberg <martin_o_lundberg@hotmail.com>
2020-08-31 13:38:50 +0200

    add a Gridview.builder with circles

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,54 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Dithmarscher Soundboard',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.green,
       ),
-      home: Container(),
+      home: MyHomePage(title: 'Dithmarscher Soundboard'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisSpacing: 15,
+          crossAxisSpacing: 15,
+        ),
+        itemBuilder: (BuildContext context, int index) => GestureDetector(
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(100.0)),
+            child: Center(
+              child: Text('${index + 1}'),
+            ),
+          ),
+          onTap: () {
+            if (mounted) {
+              setState(() {
+                print(index);
+              });
+            }
+          },
+        ),
+        primary: false,
+        padding: const EdgeInsets.all(30),
+      ),
     );
   }
 }
